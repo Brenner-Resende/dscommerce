de98a5e82d54029ad63fdcebf4c4b24d56dcc9b5
Brenner Resende Borges <brenner.resende@rocketmail.com>
2025-02-12 14:42:22 -0300

    Order, Enum, relacionamento muitos-para-um

--- src/main/java/com/brennerresende/dscommerce/entities/User.java
+++ src/main/java/com/brennerresende/dscommerce/entities/User.java
@@ -1,12 +1,15 @@
 package com.brennerresende.dscommerce.entities;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -21,6 +24,8 @@ public class User {
 	private LocalDate birthDate;
 	private String password;
 	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public User() {
 	}
@@ -84,7 +89,10 @@ public class User {
 		this.password = password;
 	}
 
-
+	public List<Order> getOrders(){
+		return orders;
+	}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
