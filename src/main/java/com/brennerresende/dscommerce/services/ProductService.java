16970620f0d6cb22ffac00a42a78cb911cd11abd
Brenner Resende Borges <brenner.resende@rocketmail.com>
2025-02-14 10:16:41 -0300

    Criando DTO e estruturando camadas

---
+++ src/main/java/com/brennerresende/dscommerce/services/ProductService.java
@@ -0,0 +1,23 @@
+package com.brennerresende.dscommerce.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.brennerresende.dscommerce.dto.ProductDTO;
+import com.brennerresende.dscommerce.entities.Product;
+import com.brennerresende.dscommerce.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+
+	@Autowired
+	private ProductRepository repository;
+	
+	@Transactional(readOnly = true)
+	public ProductDTO findById(Long id) {
+		Product product = repository.findById(id).get();
+		return new ProductDTO(product);
+		
+	}
+}
