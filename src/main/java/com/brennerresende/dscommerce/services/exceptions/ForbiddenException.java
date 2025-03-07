51393ca26590c186bb7e50789f86a13ec9271173
Brenner Resende Borges <brenner.resende@rocketmail.com>
2025-03-07 10:08:20 -0300

    Controle de acesso programático self ou admin

---
+++ src/main/java/com/brennerresende/dscommerce/services/exceptions/ForbiddenException.java
@@ -0,0 +1,9 @@
+package com.brennerresende.dscommerce.services.exceptions;
+
+public class ForbiddenException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+
+	public ForbiddenException(String msg) {
+		super(msg);
+	}
+}
